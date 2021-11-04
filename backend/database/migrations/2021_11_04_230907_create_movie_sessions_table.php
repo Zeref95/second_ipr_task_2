f31203921e8dfbb94b99998a4e49194b47c78221
viktor.niderman <viktor.niderman@gmail.com>
2021-11-05 02:44:19 +0300

    create tables

---
+++ backend/database/migrations/2021_11_04_230907_create_movie_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovieSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('city_id');
+            $table->bigInteger('movie_id');
+            $table->date('date');
+            $table->json('plates');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_sessions');
+    }
+}
